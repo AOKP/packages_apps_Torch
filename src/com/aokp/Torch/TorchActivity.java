70a23cf2ff106895dd00de747f7b55e322131137
Arnav Gupta <championswimmer@aokp.co>
2014-02-06 01:43:46 +0530

    Torch: rewrite WhiteScreen functionality
      1. Handle it inside TorchActivity instead of creating
         a new activity unnecessarily
    
      2. Exit white screen by pressing Vol+ or Vol- key
    
      3. Make WhiteScreen immersive
    
      4. Allow coming into WhiteScreen for devices that
         have hardware torches too (using an extra
         Boolean in the intent called "whitescreen")
    
    Change-Id: I5a30029dde0035ec98c8258ad382cda74deb7378
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- src/com/aokp/Torch/TorchActivity.java
+++ src/com/aokp/Torch/TorchActivity.java
@@ -1,38 +1,82 @@
 
 package com.aokp.Torch;
 
-import javax.microedition.khronos.opengles.GL10;
-
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.SurfaceTexture;
-import android.provider.Settings;
-import android.util.Log;
-import android.os.Bundle;
-
 import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Toast;
+
+
+import javax.microedition.khronos.opengles.GL10;
 
 public class TorchActivity extends Activity {
     public static final String TAG = "TorchActivity";
     private boolean mUseCameraInterface, mHasNoHardwareFlash;
+    private boolean forceWhiteScreen, mWhiteScreen;
     private TorchApp mTorchApp;
     private SurfaceTexture mSurfaceTexture;
+    private int brightness = 0;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
         mUseCameraInterface = getResources().getBoolean(R.bool.useCameraInterface);
         mHasNoHardwareFlash = getResources().getBoolean(R.bool.hasNoHardwareFlash);
+        forceWhiteScreen = getIntent().getBooleanExtra("whitescreen", false);
+        mWhiteScreen = (forceWhiteScreen || mHasNoHardwareFlash);
         boolean torchStatus = Settings.AOKP.getBoolean(getContentResolver(),
                 Settings.AOKP.TORCH_STATE, false);
-        mTorchApp = (TorchApp) getApplicationContext();
-        if (mHasNoHardwareFlash) {
-            Intent intent = new Intent(this, WhiteScreen.class);
-            startActivity(intent);
+
+        if (mWhiteScreen) {
+            setContentView(R.layout.whitescreen);
+            setTheme(R.style.WhiteScreenTheme);
+            getActionBar().hide();
+            View whiteContent = findViewById(R.id.whitescreen_content);
+            whiteContent.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast info = Toast.makeText(TorchActivity.this, R.string.toast_whitescreen, Toast.LENGTH_SHORT);
+                    info.show();
+                }
+            });
+            if (!torchStatus) {
+                try {
+                    brightness = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
+                } catch (Settings.SettingNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Settings.System.putInt(getContentResolver(),
+                        android.provider.Settings.System.SCREEN_BRIGHTNESS, 255);
+
+                View decorView = getWindow().getDecorView();
+                decorView.setBackgroundColor(Color.WHITE);
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                Settings.AOKP.putBoolean(getContentResolver(),
+                        Settings.AOKP.TORCH_STATE, true);
+
+            } else {
+                exitWhite();
+            }
+
         }
         else {
+            setContentView(R.layout.main);
+            setTheme(R.style.TransparentTheme);
+            mTorchApp = (TorchApp) getApplicationContext();
             if (mUseCameraInterface) {
                 int[] textures = new int[1];
                 GLES20.glGenTextures(1, textures, 0);
@@ -65,4 +109,65 @@ public class TorchActivity extends Activity {
 
         }
     }
+
+    public void exitWhite() {
+        if (brightness > 0) {
+            Settings.System.putInt(getContentResolver(),
+                    android.provider.Settings.System.SCREEN_BRIGHTNESS, brightness);
+        }
+        Settings.AOKP.putBoolean(getContentResolver(),
+                Settings.AOKP.TORCH_STATE, false);
+        finish();
+        android.os.Process.killProcess( android.os.Process.myPid() );
+    }
+
+    @Override
+    public void onPause () {
+        if (mWhiteScreen)
+            exitWhite();
+    }
+
+    @Override
+    public void onStop () {
+        if (mWhiteScreen)
+            exitWhite();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mHasNoHardwareFlash)
+            exitWhite();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (mWhiteScreen) {
+            if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+                    || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+                event.startTracking();
+                return true;
+            }  else {
+                return super.onKeyDown(keyCode, event);
+            }
+        } else {
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (mWhiteScreen) {
+            if ((keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+                    || (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+                event.startTracking();
+                exitWhite();
+                return true;
+            }  else {
+                return super.onKeyUp(keyCode, event);
+            }
+        } else {
+            return super.onKeyUp(keyCode, event);
+        }
+    }
+
 }
